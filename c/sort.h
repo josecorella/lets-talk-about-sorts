25233a5acff8f6c25e896985f6a52ba9281cdab3
Jose <joseeduardocorella@gmail.com>
2021-01-02 12:31:03 -0800

    Updating makefile and header files

---
+++ c/sort.h
@@ -0,0 +1,31 @@
+/* sort.h */
+
+#ifndef _NTCALC_H
+#define _NTCALC_H
+
+/* sort.c */
+
+#include <ctype.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void selection_sort(int *arr, int len);
+
+void bubble_sort(int *arr, int len);
+
+void insertion_sort(int *arr, int len);
+
+void merge_sort(int arr*, int len);
+
+void quick_sort(int *arr, int high, int low);
+
+void heap_sort(int *arr, int len);
