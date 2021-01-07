72698228cb723d54dc1563ef551cc12bfa650b78
Jose <joseeduardocorella@gmail.com>
2021-01-07 14:00:13 -0800

    Selection Sort finished

--- go/driver.go
+++ go/driver.go
@@ -5,6 +5,8 @@ import (
 )
 
 func main() {
-	sel_sort := []int {4, 10, 2, 708, 66, 44, 1, 23, 7, 2}
-    sorts.PrintArray("Selection Sort", sel_sort)
+	selSort := []int {4, 10, 2, 708, 66, 44, 1, 23, 7, 2}
+    sorts.PrintArray("Before Selection Sort", selSort)
+	sorts.SelectionSort(selSort)
+	sorts.PrintArray("After Selection Sort", selSort)
 }
--- go/sorts/sorts.go
+++ go/sorts/sorts.go
@@ -3,16 +3,21 @@ package sorts
 import "fmt"
 
 func SelectionSort(list []int) {
+    var MinIndex int
     for i := 0; i < len(list); i++ {
+        MinIndex = i
         for j := i + 1; j < len(list); j++ {
-            if 
+            if list[MinIndex] > list[j] {
+                MinIndex = j
+            }
         }
+        list[MinIndex], list[i] = list[i], list[MinIndex]
     }
 }
 
 
-func PrintArray(sort_name string, list []int)  {
-    fmt.Printf("%s: ", sort_name)
+func PrintArray(sortName string, list []int)  {
+    fmt.Printf("%s: ", sortName)
     for v := range list {
         fmt.Printf("%d ", list[v])
     }
