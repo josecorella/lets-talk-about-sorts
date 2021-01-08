03eedb4981289b92d3a8904e7ad25657878da889
Jose <joseeduardocorella@gmail.com>
2021-01-08 13:52:42 -0800

    Bubble Sort go implementation

--- go/driver.go
+++ go/driver.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+    "fmt"
 	"./sorts"
 )
 
 func main() {
-	selSort := []int {4, 10, 2, 708, 66, 44, 1, 23, 7, 2}
+	// Selection Sort
+    selSort := []int {4, 10, 2, 708, 66, 44, 1, 23, 7, 2}
     sorts.PrintArray("Before Selection Sort", selSort)
 	sorts.SelectionSort(selSort)
 	sorts.PrintArray("After Selection Sort", selSort)
+
+    fmt.Println();
+
+    // Bubble Sort
+    bubbleSort := []int {4, 10, 2, 7, 99, 100, 1, 42, 34, 23}
+    sorts.PrintArray("Before Bubble Sort", bubbleSort)
+	sorts.BubbleSort(bubbleSort)
+	sorts.PrintArray("After Bubble Sort", bubbleSort)
+
 }
--- go/sorts/sorts.go
+++ go/sorts/sorts.go
@@ -15,6 +15,16 @@ func SelectionSort(list []int) {
     }
 }
 
+func BubbleSort(list []int) {
+    for i := 0; i < len(list); i++ {
+        for j := 0; j < len(list) - i - 1; j++ {
+            if list[j] > list[j + 1] {
+                list[j], list[j+1] = list[j+1], list[j]
+            }
+        }
+    }
+}
+
 
 func PrintArray(sortName string, list []int)  {
     fmt.Printf("%s: ", sortName)
